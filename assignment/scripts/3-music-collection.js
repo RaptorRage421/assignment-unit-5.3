4becc9944dafa51d8558f94101d4b35adc134b58
Sam Shacter <sshacter@gmail.com>
2024-04-26 00:08:26 -0500

    still working on the addToCollection test failing.seem to have lost the showCollection() test passing at some point to.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,42 +3,49 @@ console.log('***** Music Collection *****')
 // setting up a blank collection
 const myCollection = [];
 
+
 // creating a new function 'addToCollection', with input variables 
 // collection, title, artist, and yearPublished
 // 
+//make a function where the collection input becomes an album object with a stored title, artist and yearPublished
+
+
 function addToCollection(collection, title, artist, yearPublished) {
-  let primesCollection = {};
-  primesCollection.collection = collection;
-  //console.log('Album: ' , collection);
-  primesCollection.title = title;
-  //console.log('Title: ', title);
-  primesCollection.artist = artist;
-  //console.log('Artist: ', artist)
-  primesCollection.yearPublished = yearPublished;
-  //console.log('Year Published: ', yearPublished);
+  console.log('In addToCollection - adding ', collection);
+  let newCollection = {};
+  let album = collection;
+  newCollection[0] = collection + ': ';
+  newCollection.title = title;
+  newCollection.artist = artist;
+  newCollection.yearPublished = yearPublished;
+  myCollection.push(newCollection);
+  return myCollection[myCollection.length-1];
+  
+  
+  
+  
+  
+  // let newCollection = collection;
+  // album.push(collection);
+  // addCollection = {};
+  // collection = addCollection;
+  
 
-  myCollection.push(primesCollection);
-  return myCollection;
+  // //console.log('Album: ' , collection);
+  // addCollection.title = title;
+  // //console.log('Title: ', title);
+  // addCollection.artist = artist;
+  // //console.log('Artist: ', artist)
+  // addCollection.yearPublished = yearPublished;
+  // //console.log('Year Published: ', yearPublished);
+ 
+  // myCollection.push(addCollection);
+  // return newCollection;
 }
-console.log(myCollection.collection)
-// function empty() {
-//   myCollection.length = 0;
-//   return 'Emptied the Collection';
-// }
 
-function showCollection() {
-  for (i = 0; i < myCollection.length; i++) {
-    console.log(`${myCollection[i].title} by ${myCollection[i].artist}, published in ${myCollection[i].yearPublished}, on ${myCollection[i].collection} `);
-  }
-  return myCollection
-}
-
-function findByArtist(collection, artist) {
-
-
-}
 
 console.log(addToCollection('Strobelite Seduction', 'Angel On My Shoulder', 'Kaskade', '2008'));
+console.log(addToCollection('Strobelite Seduction', 'Move for Me', 'Kaskade', '2008'));
 console.log(addToCollection('Elevate', 'The Longest Road', 'Morgan Page', '2008'));
 console.log(addToCollection('Armada Music', 'Lose This Feeling (Dimenion Remix)', 'Armin van Buuren' + ', ' + 'Dimension', '2023'));
 console.log(addToCollection('Atlantic Records UK', 'Wasted Youth (Original Mix)', 'goddard.' + ', ' + 'CatBurns', '2024'));
@@ -47,8 +54,26 @@ console.log(addToCollection('Rave Room Recordings', "Vibin'", 'Kide (IT)', '2024
 console.log(addToCollection('Axtone Records', 'Tell Me Why (1991 Remix)', 'Supermode' + ', ' + '1991', '2023'));
 console.log(addToCollection('b2s Compilations', 'DLMD (Original Mix)', 'Darren Styles' + ', ' + 'TNT', '2024'))
 
-console.log('Testing showCollection: ', showCollection());
+// function empty() {
+//   myCollection.length = 0;
+//   return 'Emptied the Collection';
+// }
 
+function showCollection() {
+  console.log('in showCollection');
+  for (i = 0; i < myCollection.length; i++) {
+    console.log(`${myCollection[i].title} by ${myCollection[i].artist}, published in ${myCollection[i].yearPublished}`);
+  }
+  return myCollection;
+}
+
+function findByArtist(collection, artist) {
+
+
+}
+
+
+console.log('Testing showCollection: ', showCollection());
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
