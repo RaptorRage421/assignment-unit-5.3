7b1ec5186b55c3ebdb007a0c6e07515811c4c5b4
Sam Shacter <sshacter@gmail.com>
2024-04-26 14:27:57 -0500

    worked through the test passing with classmates. now working on the findByArtist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,7 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 // setting up a blank collection
-const myCollection = [];
+let myCollection = [];
 
 
 // creating a new function 'addToCollection', with input variables 
@@ -11,15 +11,19 @@ const myCollection = [];
 
 
 function addToCollection(collection, title, artist, yearPublished) {
-  console.log('In addToCollection - adding ', collection);
-  let newCollection = {};
-  let album = collection;
-  newCollection[0] = collection + ': ';
-  newCollection.title = title;
-  newCollection.artist = artist;
-  newCollection.yearPublished = yearPublished;
-  myCollection.push(newCollection);
-  return myCollection[myCollection.length-1];
+  collection.push({artist, title, yearPublished})
+  return collection[5];
+}
+
+  // console.log('In addToCollection - adding ', collection);
+  // let newCollection = {};
+  
+  // newCollection.album = collection;
+  // newCollection.title = title;
+  // newCollection.artist = artist;
+  // newCollection.yearPublished = yearPublished;
+  // myCollection.push(newCollection);
+  // return newCollection;
   
   
   
@@ -41,39 +45,39 @@ function addToCollection(collection, title, artist, yearPublished) {
  
   // myCollection.push(addCollection);
   // return newCollection;
-}
 
 
-console.log(addToCollection('Strobelite Seduction', 'Angel On My Shoulder', 'Kaskade', '2008'));
-console.log(addToCollection('Strobelite Seduction', 'Move for Me', 'Kaskade', '2008'));
-console.log(addToCollection('Elevate', 'The Longest Road', 'Morgan Page', '2008'));
-console.log(addToCollection('Armada Music', 'Lose This Feeling (Dimenion Remix)', 'Armin van Buuren' + ', ' + 'Dimension', '2023'));
-console.log(addToCollection('Atlantic Records UK', 'Wasted Youth (Original Mix)', 'goddard.' + ', ' + 'CatBurns', '2024'));
-console.log(addToCollection('Relentless Records', 'Toxic (Phibes Remix)', 'Songer' + ', ' + 'Phibes', '2024'));
-console.log(addToCollection('Rave Room Recordings', "Vibin'", 'Kide (IT)', '2024'));
-console.log(addToCollection('Axtone Records', 'Tell Me Why (1991 Remix)', 'Supermode' + ', ' + '1991', '2023'));
-console.log(addToCollection('b2s Compilations', 'DLMD (Original Mix)', 'Darren Styles' + ', ' + 'TNT', '2024'))
+
+console.log(addToCollection(myCollection, 'Angel On My Shoulder', 'Kaskade', '2008'));
+console.log(addToCollection(myCollection, 'Move for Me', 'Kaskade', '2008'));
+console.log(addToCollection(myCollection, 'The Longest Road', 'Morgan Page', '2008'));
+console.log(addToCollection(myCollection, 'Lose This Feeling (Dimenion Remix)', 'Armin van Buuren' + ', ' + 'Dimension', '2023'));
+console.log(addToCollection(myCollection, 'Wasted Youth (Original Mix)', 'goddard.' + ', ' + 'CatBurns', '2024'));
+console.log(addToCollection(myCollection, 'Toxic (Phibes Remix)', 'Songer' + ', ' + 'Phibes', '2024'));
+console.log(addToCollection(myCollection, "Vibin'", 'Kide (IT)', '2024'));
+console.log(addToCollection(myCollection, 'Tell Me Why (1991 Remix)', 'Supermode' + ', ' + '1991', '2023'));
+console.log(addToCollection(myCollection, 'DLMD (Original Mix)', 'Darren Styles' + ', ' + 'TNT', '2024'))
 
 // function empty() {
 //   myCollection.length = 0;
 //   return 'Emptied the Collection';
 // }
 
-function showCollection() {
-  console.log('in showCollection');
-  for (i = 0; i < myCollection.length; i++) {
-    console.log(`${myCollection[i].title} by ${myCollection[i].artist}, published in ${myCollection[i].yearPublished}`);
+function showCollection(collection){
+  for (i=0; i<collection.length; i++){
+  console.log(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}`);
   }
-  return myCollection;
+  
 }
 
-function findByArtist(collection, artist) {
+// function findByArtist(collection, artist) {
 
 
-}
+// }
 
 
-console.log('Testing showCollection: ', showCollection());
+console.log('Testing showCollection: ', showCollection(myCollection));
+
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
