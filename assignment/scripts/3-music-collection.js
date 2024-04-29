4faf7969082628ff159492a6a370cebd6897b0b7
Sam Shacter <sshacter@gmail.com>
2024-04-28 19:02:28 -0500

    made a change to the search, if they search only by a song name, if searchCriteria === string, so no need to enter the name/yearpublished object into the search

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,20 +11,22 @@ let myCollection = [];
 /*
 added new input trackers, this should always be an array. i have tried to break it down to [name, duration],
 but it seems to break part of the testing each time. will continue to play with it. 
-
+updated the input to take name and duration separately instead of requiring an array. 
+tracks are added as an object to make the .name and .duration attribute call
 */
-function addToCollection(collection, title, artist, yearPublished, trackers) { //...[track,duration]
+function addToCollection(collection, title, artist, yearPublished, name, duration) { //...[track,duration]
   let addHelper = {};
-  let tracker = [];
-  tracker = trackers;
+  // let tracker = [];
+  // let trackers = {};
+  tracker = [name, duration];
   /*
   check to see if the Album already exists,
   if it does, splice the new tracks into the album in the collection
   */
   for (i = 0; i < collection.length; i++) {
     if (collection[i].title === title) {
-      addHelper.tracks = tracker;
-      collection[i].tracks.splice(2, 0, trackers);
+      //addHelper.tracks = {name: name, duration: duration};
+      collection[i].tracks.splice(0, 0, { name: name, duration: duration });
       return;
     }
   }
@@ -33,12 +35,12 @@ function addToCollection(collection, title, artist, yearPublished, trackers) { /
   then push addHelper into the collection
   */
   addHelper = {};
-  tracker = trackers;
-  console.log('in addToCollection');
+  // tracker = trackers;
+  // console.log('in addToCollection');
   addHelper.title = title;
   addHelper.artist = artist;
   addHelper.yearPublished = yearPublished;
-  addHelper.tracks = [tracker];
+  addHelper.tracks = [{ name: name, duration: duration }];
   collection.push(addHelper);
   return addHelper;
 
@@ -57,19 +59,19 @@ function addToCollection(collection, title, artist, yearPublished, trackers) { /
 actually add files to the collection
 */
 
-addToCollection(myCollection, 'Strobelite Seduction', 'Kaskade', '2008', ['Move For Me', '3:57']); // move for me 3:57 / Angel on My Shoulder 3:47 / I Remember (Strobelite Edit) 4:43
-addToCollection(myCollection, 'Strobelite Seduction', 'Kaskade', '2008', ['Angel on My Shoulder', '3:47']);
-addToCollection(myCollection, 'Strobelite Seduction', 'Kaskade', '2008', ['I Remember (Strobelite Edit)', '4:43']);
-addToCollection(myCollection, 'Dynasty', 'Kaskade', '2010', ['Fire in Your Shoes', '2:39']);
-addToCollection(myCollection, 'Nettwerk Music Group', 'Morgan Page', '2014', ['In The Air', '7:06']);
-addToCollection(myCollection, 'Nettwerk Music Group', 'Morgan Page', '2014', ['Against the World', '6:26'])// In The Air feat Angela McCluskey Extended Mix 7:06 / Against the World Original Mix 6:26 
-addToCollection(myCollection, 'Armada Music', 'Armin van Buuren', '2023', ['Lose This Feeling (Dimension Remix)', '3:52']);
-addToCollection(myCollection, 'Armada Music', 'Armin van Buuren', '2023', ['Lose This Feeling (Maddix Remix)', '4:43']);
-addToCollection(myCollection, 'Armada Music', 'Armin van Buuren', '2023', ['Make It Count', '5:04']);   // Lose This Feeling (Dimenion Remix) 3:52 / Lose This Feeling (Maddix Extended Remix) 4:43 / Make It Count 5:04
-addToCollection(myCollection, 'Atlantic Records UK', 'goddard.', '2024', ['Wasted Youth (Original Mix)', '2:27']);
-addToCollection(myCollection, 'Atlantic Records UK', 'goddard.', '2024', ['Wasted Youth [IVY] Remix', '3:09']);// Wasted Youth feat. CatBurns (Original Mix) 2:27 / Wasted Youth feat. CatBurns [IVY] Remix 3:09
-addToCollection(myCollection, 'Rave Room Recordings', 'Kide (IT)', '2024', ["Vibin'", '4:10']);  //Vibin' 4:10
-addToCollection(myCollection, 'Axtone Records', 'Supermode', '2023', ['Tell Me Why (1991 Remix)', '4:04']); // Tell Me Why (1991 Remix) 4:04
+addToCollection(myCollection, 'Strobelite Seduction', 'Kaskade', '2008', 'Move For Me', '3:57'); // move for me 3:57 / Angel on My Shoulder 3:47 / I Remember (Strobelite Edit) 4:43
+addToCollection(myCollection, 'Strobelite Seduction', 'Kaskade', '2008', 'Angel on My Shoulder', '3:47');
+addToCollection(myCollection, 'Strobelite Seduction', 'Kaskade', '2008', 'I Remember (Strobelite Edit)', '4:43');
+addToCollection(myCollection, 'Dynasty', 'Kaskade', '2008', 'Fire in Your Shoes', '2:39');
+addToCollection(myCollection, 'Nettwerk Music Group', 'Morgan Page', '2014', 'In The Air', '7:06');
+addToCollection(myCollection, 'Nettwerk Music Group', 'Morgan Page', '2014', 'Against the World', '6:26')// In The Air feat Angela McCluskey Extended Mix 7:06 / Against the World Original Mix 6:26 
+addToCollection(myCollection, 'Armada Music', 'Armin van Buuren', '2023', 'Lose This Feeling (Dimension Remix)', '3:52');
+addToCollection(myCollection, 'Armada Music', 'Armin van Buuren', '2023', 'Lose This Feeling (Maddix Remix)', '4:43');
+addToCollection(myCollection, 'Armada Music', 'Armin van Buuren', '2023', 'Make It Count', '5:04');   // Lose This Feeling (Dimenion Remix) 3:52 / Lose This Feeling (Maddix Extended Remix) 4:43 / Make It Count 5:04
+addToCollection(myCollection, 'Atlantic Records UK', 'goddard.', '2024', 'Wasted Youth (Original Mix)', '2:27');
+addToCollection(myCollection, 'Atlantic Records UK', 'goddard.', '2024', 'Wasted Youth [IVY] Remix', '3:09');// Wasted Youth feat. CatBurns (Original Mix) 2:27 / Wasted Youth feat. CatBurns [IVY] Remix 3:09
+addToCollection(myCollection, 'Rave Room Recordings', 'Kide (IT)', '2024', "Vibin'", '4:10');  //Vibin' 4:10
+addToCollection(myCollection, 'Axtone Records', 'Supermode', '2023', 'Tell Me Why (1991 Remix)', '4:04'); // Tell Me Why (1991 Remix) 4:04
 
 // console.log(myCollection)
 // function empty() {
@@ -86,14 +88,14 @@ this requires a nested loop where we'll have "i" as a variable for the outerloop
 this allows us to scan through the tracks for each album, and only list the album once. 
 */
 function showCollection(collection) {
-  console.log('This is my collection: ', collection)
+  // console.log('This is my collection: ', collection)
   for (let i = 0; i < collection.length; i++) {
     console.log(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}: `);
     for (let j = 0; j < collection[i].tracks.length; j++) {
-      console.log(`     ${j + 1}. NAME: ${collection[i].tracks[j][0]} DURATION: ${collection[i].tracks[j][1]}`)
+      console.log(`   ${j + 1}. NAME: ${collection[i].tracks[j].name} DURATION: ${collection[i].tracks[j].duration}`)
     }
   }
-
+  return collection;
 }
 
 /*
@@ -103,6 +105,7 @@ then return all the songs by that artist
 */
 
 function findByArtist(collection, artist) {
+  console.log('Find Albums by Artist: ', artist);
   let newArray = [];
   for (i = 0; i < collection.length; i++) {
     if (collection[i].artist === artist) {
@@ -115,7 +118,8 @@ function findByArtist(collection, artist) {
 some console log tests to check the functions findByArtist and showCollection
 */
 console.log('Testing Find Artist:', findByArtist(myCollection, 'Kaskade'));
-console.log('Testing showCollection: ', showCollection(myCollection));
+showCollection(myCollection)
+// console.log('Testing showCollection: ', showCollection(myCollection));
 /*
 stretch goal: search function to search against artist and yearPublished
 adding the extra stretch search for track name. adding optional parameter, to use if there, otherwise as normal
@@ -130,11 +134,11 @@ function search(collection, searchCriteria, trackName) {
   //console.log('in search');
   let searchArray = [];
   let searchObject = {};
-  if (trackName) {
+  if (typeof searchCriteria === 'string' || trackName ) {
     for (let i = 0; i < collection.length; i++) {
       for (let j = 0; j < collection[i].tracks.length; j++) {
-        if (collection[i].tracks[j][0] === trackName) {
-          console.log('input parameters are: ', trackName)
+        if (collection[i].tracks[j].name === trackName || collection[i].tracks[j].name === searchCriteria) {
+          console.log('Searching by song: ', trackName, searchCriteria)
           searchObject = collection[i];
           searchArray.push(searchObject);
           return searchArray;
@@ -149,13 +153,14 @@ function search(collection, searchCriteria, trackName) {
   */
   if (searchCriteria) {
     if (typeof searchCriteria !== 'object' || searchCriteria.yearPublished === undefined || searchCriteria.artist === undefined || searchCriteria === undefined || searchCriteria.artist.length === 0 || searchCriteria.yearPublished.length === 0) {
-      console.log('input parameters are: ', searchCriteria);
+      console.log('Search parameters are: ', searchCriteria);
       return collection;
     }
-  /*
-  if the search is entered correctly, for the length of the collection, check the objects to see if the artist and yearPublished strictly match.
-  if they do, set searchObject to that index of the collection, and push the object to searchArray
-  */  
+    /*
+    if the search is entered correctly, for the length of the collection, check the objects to see if the artist and yearPublished strictly match.
+    if they do, set searchObject to that index of the collection, and push the object to searchArray
+    return searchArray
+    */
     else {
       for (i = 0; i < collection.length; i++) {
 
@@ -178,11 +183,13 @@ console.log('Search Test - Testing no artist only year: ', search(myCollection, 
 console.log('Search Test: - Testing incorrect date: ', search(myCollection, { artist: 'Songer', yearPublished: '2020' }));
 console.log('Search Test: - Testing no input criteria: ', search(myCollection));
 console.log('Search Test: - empty artist string', search(myCollection, { artist: '', yearPublished: '2020' }));
+console.log('Search Test: - no artist string', search(myCollection, { yearPublished: '2020' }));
 console.log('Search Test - not an object(number): ', search(myCollection, 2008));
 console.log('Search Test - not an object(string): ', search(myCollection, '2008'));
 console.log('Search Test: Testing Song Name: ', search(myCollection, { artist: 'Kaskade', yearPublished: '2008' }, 'Tell Me Why (1991 Remix)'))
-console.log('Search Test - song title again: ', search(myCollection, { yearPublished: '2024' }, 'In The Air'));
-
+console.log('Search Test - song title again: ', search(myCollection, 'In The Air'));
+console.log('Search Test: song title only no artist/year input: ', search(myCollection, 'Wasted Youth [IVY] Remix'))
+console.log('Search Test: - no yearPublished string (expecting full collection)', search(myCollection, { artist: 'Armin van Buuren' }));
 
 
 //console.log('Search Test: ', search(myCollection));
